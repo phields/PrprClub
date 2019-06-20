66a85ba50cf496727179bcd399ea9b7ae6fcca84
Afanyiyu <afanyiyu@hotmail.com>
2019-06-20 21:48:36 +0800

    替换标题。

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,10 +2,10 @@
 <el-container>
   <el-main>
     <el-menu :default-active="activeIndex" mode="horizontal" router class="menu">
-        <el-menu-item>
+        <el-menu-item index="/dashboard/me">
           <svg-icon iconClass="logoPink" width="134.34" height="60"/>
         </el-menu-item>
-      <el-menu-item index="/dashboard/me">我</el-menu-item>
+      <!-- <el-menu-item index="/dashboard/me">我</el-menu-item> -->
       <el-menu-item index="/dashboard/works">作品</el-menu-item>
       <el-menu-item index="/dashboard/projects">企划</el-menu-item>
       <el-menu-item index="/dashboard/messages">消息</el-menu-item>
--- src/views/Discover.vue
+++ src/views/Discover.vue
@@ -3,10 +3,10 @@
     <el-main>
       <img src="@/assets/bvtb.jpeg" class="headPic">
       <el-menu :default-active="activeIndex" mode="horizontal" router class="menu">
-        <el-menu-item>
+        <el-menu-item index="/discover/home">
           <svg-icon iconClass="logoBlue" width="134.34" height="60"/>
         </el-menu-item>
-        <el-menu-item index="/discover/home">发现</el-menu-item>
+        <!-- <el-menu-item index="/discover/home">发现</el-menu-item> -->
         <el-menu-item index="/discover/dynamics">动态</el-menu-item>
         <el-menu-item index="/discover/adoption">领养</el-menu-item>
         <el-menu-item index="/discover/about">关于我们</el-menu-item>
