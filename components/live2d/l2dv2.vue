13cd3e0ad66873ea33935c5f3ee0313021b04d47
Afanyiyu <afanyiyu@hotmail.com>
2019-06-25 18:59:16 +0800

    添加了Live2D呈现器，请在Components.md中阅读使用方法。

---
+++ components/live2d/l2dv2.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <canvas id="glcanvas"
+            :width="width"
+            :height="height"
+            style="border:dashed 1px #CCC">
+    </canvas>
+  </div>
+</template>
+
+<script>
+import '~/renderer/live2d/live2d.min.js'
+import '~/renderer/live2d/Live2DFramework.js'
+import { sampleApp1 } from '~/renderer/live2d/live2d_sdk2.js'
+export default {
+  name: 'l2dv2',
+  props: ['height', 'width'],
+  created () {
+    sampleApp1()
+    // AT: BACK_IMAGE_NAME: "assets/image/back_class_normal.png",
+    // AT: MODEL_HARU: "assets/live2d/mod/mod.model.json",
+  }
+}
+</script>
+
+  <style>
+html,
+body {
+  oveflow: hidden;
+  height: 100%;
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
+#glcanvas {
+  /*
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                */
+  background-size: 150%;
+  background-position: 50% 50%;
+  background-image: url(assets/image/back_class_normal.png);
+}
+
+button {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 14px;
+  color: #ffffff;
+  padding: 10px 20px;
+  -moz-border-radius: 30px;
+  -webkit-border-radius: 30px;
+  border-radius: 30px;
+  -moz-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.5),
+    inset 0px 0px 1px rgba(255, 255, 255, 0.7);
+  -webkit-box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.5),
+    inset 0px 0px 1px rgba(255, 255, 255, 0.7);
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.5),
+    inset 0px 0px 1px rgba(255, 255, 255, 0.7);
+  text-shadow: 0px -1px 0px rgba(0, 0, 0, 0.4),
+    0px 1px 0px rgba(255, 255, 255, 0.3);
+}
+
+button.active {
+  background: linear-gradient(to bottom, #3498db, #2980b9);
+  background: -moz-linear-gradient(top, #3498db 0%, #2980b9);
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(#3498db),
+    to(#2980b9)
+  );
+  border: 1px solid #2980b9;
+}
+
+button.inactive {
+  background: linear-gradient(to bottom, #e74c3c, #c0392b);
+  background: -moz-linear-gradient(top, #e74c3c 0%, #c0392b);
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    from(#e74c3c),
+    to(#c0392b)
+  );
+  border: 1px solid #c0392b;
+}
+</style>
---
+++ components/live2d/l2dv3.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <canvas id="SAMPLE"
+            :width="width"
+            :height="height">
+      Live2D Canvas未正确加载。
+    </canvas>
+  </div>
+</template>
+
+<script>
+import 'https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js'
+import '~/renderer/live2d/live2dcubismcore.min.js'
+import { main } from '~/renderer/live2d/live2d_sdk3.js'
+export default {
+  name: 'l2dv3',
+  props: ['height', 'width'],
+  created () {
+    main()
+    // AT: './res/mod/mod.model3.json'
+  }
+}
+</script>
